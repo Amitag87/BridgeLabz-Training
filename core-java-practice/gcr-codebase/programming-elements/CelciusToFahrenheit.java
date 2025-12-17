63f0e3046f2019b01fc4d7e092fe37f59cf29904
AMIT AGARWAL <amit.agarwal_cs22@gla.ac.in>
2025-12-17 15:55:55 +0530

    [Amit Agarwal] Celcius to Fahrenheit

--- core-java-practice/gcr-codebase/programming-elements/CelciusToFahrenheit.java
+++ core-java-practice/gcr-codebase/programming-elements/CelciusToFahrenheit.java
@@ -3,8 +3,8 @@ public class Main{
 	public static void main(String[] args){
 	Scanner sc=new Scanner(System.in);
 	int tempC = sc.nextInt();
-	int fahrenheit = (tempC * 9/5)+32;
-	System.out.print(fahrenheit);
+	int Fahrenheit = (tempC * 9/5)+32;
+	System.out.print(Fahrenheit);
 
 }
 }
