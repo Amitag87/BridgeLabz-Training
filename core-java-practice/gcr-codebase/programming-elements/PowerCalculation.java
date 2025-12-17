451d03c4fe72223da581698bf3efbe0030b40176
AMIT AGARWAL <amit.agarwal_cs22@gla.ac.in>
2025-12-17 16:22:40 +0530

    [Amit Agarwal] Power Calculation

---
+++ core-java-practice/gcr-codebase/programming-elements/PowerCalculation.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main{
+	public static void main(String[] args){
+	Scanner sc=new Scanner(System.in);
+	int value=sc.nextInt();
+	int power=sc.nextInt();
+	int powerCal=(int)Math.pow(value,pow);
+	System.out.println(powerCal);
+}
+}
