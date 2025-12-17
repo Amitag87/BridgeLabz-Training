ac873db153e427f4e660d36dc9dbf0ae9bec45e9
AMIT AGARWAL <amit.agarwal_cs22@gla.ac.in>
2025-12-17 16:29:40 +0530

    [Amit Agarwal] Convert km into miles

--- core-java-practice/gcr-codebase/programming-elements/ConvertKmToMiles.java
+++ core-java-practice/gcr-codebase/programming-elements/ConvertKmToMiles.java
@@ -3,7 +3,7 @@ public class Main{
 	public static void main(String[] args){
 	Scanner sc=new Scanner(System.in);
 	int km=sc.nextInt();
-	int miles=1.5*km;
+	int miles=0.621371*km;
 	System.out.print(miles);
 
 }
