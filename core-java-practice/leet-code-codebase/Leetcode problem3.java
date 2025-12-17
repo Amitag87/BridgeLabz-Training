8d57d40f8a3b71065e5acaf0a6148f50793ec967
AMIT AGARWAL <amit.agarwal_cs22@gla.ac.in>
2025-12-17 15:18:47 +0530

    [Amit Agarwal] Leetcode Problems

--- core-java-practice/leet-code-codebase/Leetcode problem3.java
+++ core-java-practice/leet-code-codebase/Leetcode problem3.java
@@ -1,25 +1,34 @@
-import java.util.*;
-
-public class LongestSubstringWithoutRepeating {
-    public static int main(String s) {
-	String s = "abcabcbb";
-        HashSet<Character> set = new HashSet<>();
-        int left = 0;
-        int maxLength = 0;
-
-        for (int right = 0; right < s.length(); right++) {
-            while (set.contains(s.charAt(right))) {
-                set.remove(s.charAt(left));
-                left++;
+class Solution {
+    public static void main(String args[]){
+	int nums[3]={1,2,3};
+	int nums[4]={2,3,4};
+        int n1=nums1.length , n2=nums2.length;
+        int[] nums=new int[n1+n2]; int i=0,j=0,k=0;
+        while(i<nums1.length && j<nums2.length){
+            if(nums1[i]<nums2[j]){
+                nums[k]=nums1[i]; i++;
             }
-            set.add(s.charAt(right));
-            maxLength = Math.max(maxLength, right - left + 1);
+            else{
+                nums[k]=nums2[j];
+                j++;
+            }
+            k++;
         }
-        System.out.println(maxLength);
+        while(i<nums1.length){
+            nums[k]=nums1[i];
+            i++;k++;
+        }
+        while(j<nums2.length){
+            nums[k]=nums2[j];
+            j++;k++;
+        }
+        double result=0;
+        if(nums.length%2==0){
+             result=(nums[nums.length/2]+nums[(nums.length/2)-1])/2.0;
+        }
+        else{
+            result=nums[nums.length/2];
+        }
+	System.out.print(result);
     }
-
-    
-        
-        
-    
 }
