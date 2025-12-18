ff571d9adb36f18cca16b6e53447ba10fa75aa91
AMIT AGARWAL <amit.agarwal_cs22@gla.ac.in>
2025-12-18 11:51:08 +0530

    [Amit Agarwal] Kilometer to Miles

---
+++ core-java-practice/gcr-codebase/programming-elements/KmtoMiles.java
@@ -0,0 +1,9 @@
+package BridgeLab;
+import java.util.*;
+public class KmtoMiles {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		double distInKms=sc.nextDouble();
+		System.out.print("distance in miles is "+ distInKms*0.6);
+	}
+}
