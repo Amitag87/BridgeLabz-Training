fe04d8012b7540d2bc01c444c39864f42c8417c8
AMIT AGARWAL <125277701+Amitag87@users.noreply.github.com>
2025-12-19 16:44:07 +0530

    [Amit Agarwal] Add.ControlFlowLevel1

--- core-java-practice/gcr-codebase/control-flow/EvenOddTillNum.java
+++ core-java-practice/gcr-codebase/control-flow/EvenOddTillNum.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 public class EvenOddTillNum {
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int num = scn.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
         for (int i = 1; i <= num; i++) {
             if (i % 2 == 0) {
                 System.out.println(i + " is Even");
