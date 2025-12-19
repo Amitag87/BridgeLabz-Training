f4baeb1bd151fdeec405562232aab3e4086fa30e
AMIT AGARWAL <125277701+Amitag87@users.noreply.github.com>
2025-12-19 16:36:37 +0530

    [Amit Agarwal] Add.ControlFlowLevel1

--- core-java-practice/gcr-codebase/control-flow/FactorialUsingFor.java
+++ core-java-practice/gcr-codebase/control-flow/FactorialUsingFor.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class FactorialUsingFor {
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int number = scn.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
         int factorial = 1;
         for (int i = 1; i <= number; i++) {
             factorial = factorial * i;
