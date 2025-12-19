3145f4a624dd2a7937f62a5e2543250d1a194358
AMIT AGARWAL <125277701+Amitag87@users.noreply.github.com>
2025-12-19 11:09:38 +0530

    Refactored

--- core-java-practice/gcr-codebase/programming-elements/PracticeProblem19thDec2025/Ques3.java
+++ core-java-practice/gcr-codebase/programming-elements/PracticeProblem19thDec2025/Ques3.java
@@ -2,8 +2,8 @@ import java.util.*;
 public class Main{
 	public static void main(String[] args){
 	Scanner sc=new Scanner(System.in);
-	int tempC = sc.nextInt();
-	int fahrenheit = (tempC * 9/5)+32;
+	int tempCelcius = sc.nextInt();
+	int fahrenheit = (tempCelcius * 9/5)+32;
 	System.out.print(fahrenheit);
 
 }
