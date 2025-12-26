36f72a8efb5c4fd5a0fdd84fbdaa035c55e098e9
AMIT AGARWAL <amit.agarwal_cs22@gla.ac.in>
2025-12-26 11:46:54 +0530

    [Amit Agarwal]Add.Solved extra questions of strings

---
+++ core-java-practice/gcr-codebase/extra/LongestWord.java
@@ -0,0 +1,26 @@
+package extra;
+
+public class LongestWord {
+    
+    public static String findLongestWord(String sentence) {
+        String[] words = sentence.split("\\s+");
+        String longest = "";
+        
+        for (String word : words) {
+            if (word.length() > longest.length()) {
+                longest = word;
+            }
+        }
+        
+        return longest;
+    }
+    
+    public static void main(String[] args) {
+        String sentence = "The quick brown fox jumps over the lazy dog";
+        String longestWord = findLongestWord(sentence);
+        
+        System.out.println("Sentence: " + sentence);
+        System.out.println("Longest word: " + longestWord);
+        System.out.println("Length: " + longestWord.length());
+    }
+}
