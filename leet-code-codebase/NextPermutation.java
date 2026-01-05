7f2815b6a0793683adada20e4c8c5d4d5c04ba22
AMIT AGARWAL <amit.agarwal_cs22@gla.ac.in>
2026-01-05 16:26:43 +0530

    [Amit Agarwal] Add.leetcode

---
+++ leet-code-codebase/NextPermutation.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class NextPermutation {
+
+    public String countAndSay(int n) {
+        String s = "1";
+        for (int i = 2; i <= n; i++) {
+            StringBuilder next = new StringBuilder();
+            int count = 1;
+
+            for (int j = 1; j < s.length(); j++) {
+                if (s.charAt(j) == s.charAt(j - 1)) {
+                    count++;
+                } else {
+                    next.append(count).append(s.charAt(j - 1));
+                    count = 1;
+                }
+            }
+            next.append(count).append(s.charAt(s.length() - 1));
+            s = next.toString();
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        NextPermutation sol = new NextPermutation();
+
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        System.out.println("Count and Say term: " + sol.countAndSay(n));
+    }
+}
