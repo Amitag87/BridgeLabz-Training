75699b09345d6f6cc9a73d9e8188390230f3019f
AMIT AGARWAL <amit.agarwal_cs22@gla.ac.in>
2026-01-08 16:50:52 +0530

    [Amit Agarwal] Add.LeetCode

---
+++ dsa-practice/leet-code-codebase/ReverseLinkedList.java
@@ -0,0 +1,61 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        
+        return prev;
+    }
+    
+    public static ListNode createList(int[] arr) {
+        if (arr.length == 0) return null;
+        
+        ListNode head = new ListNode(arr[0]);
+        ListNode current = head;
+        
+        for (int i = 1; i < arr.length; i++) {
+            current.next = new ListNode(arr[i]);
+            current = current.next;
+        }
+        
+        return head;
+    }
+    
+    public static void printList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val);
+            if (current.next != null) System.out.print(" -> ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+    
+    public static void main(String[] args) {
+        ReverseLinkedList solution = new ReverseLinkedList();
+        
+        int[] arr = {1, 2, 3, 4, 5};
+        ListNode head = createList(arr);
+        
+        System.out.print("Original: ");
+        printList(head);
+        
+        ListNode reversed = solution.reverseList(head);
+        System.out.print("Reversed: ");
+        printList(reversed);
+    }
+}
