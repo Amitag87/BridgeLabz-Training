f229b82f3ef0504ae6e40768c19a0d6302e848ec
AMIT AGARWAL <amit.agarwal_cs22@gla.ac.in>
2026-01-19 15:10:43 +0530

    [Amit Agarwal]Refactor.Multiple Inheritance incorrect code

--- review/MultipleInheritance.java
+++ review/MultipleInheritance.java
@@ -5,23 +5,19 @@ class A{
     }
 }
 class B{
-    void methodb(){
+    void methodA(){
         System.out.println("Method from class B");
     }
 }
-class C extends A{
-    void methodC(){
+class C extends A,B{
+    void methodA(){
         System.out.println("Method from class C extending A");
     }
 }
-class C extends B{
-    void methodC(){
-        System.out.println("Method from class C extending B");
-    }
-}
+
 public class MultipleInheritance {
     public static void main(String[] args) {
         C c = new C();
-        c.methodC();
+        c.methodA();
     }
 }
