941eda60b72668094697a8189d92470018a49586
AMIT AGARWAL <amit.agarwal_cs22@gla.ac.in>
2026-01-19 15:21:15 +0530

    [Amit Agarwal] Add.Multiple Inheritance correct code using interfaces

--- review/MultipleInheritance.java
+++ review/MultipleInheritance.java
@@ -1,23 +1,23 @@
-package review;
-class A{
-    void methodA(){
-        System.out.println("Method from class A");
-    }
-}
-class B{
-    void methodA(){
-        System.out.println("Method from class B");
-    }
-}
-class C extends A,B{
-    void methodA(){
-        System.out.println("Method from class C extending A");
-    }
-}
+// package review;
+// class A{
+//     void methodA(){
+//         System.out.println("Method from class A");
+//     }
+// }
+// class B{
+//     void methodA(){
+//         System.out.println("Method from class B");
+//     }
+// }
+// class C extends A,B{
+//     void methodA(){
+//         System.out.println("Method from class C extending A");
+//     }
+// }
 
-public class MultipleInheritance {
-    public static void main(String[] args) {
-        C c = new C();
-        c.methodA();
-    }
-}
+// public class MultipleInheritance {
+//     public static void main(String[] args) {
+//         C c = new C();
+//         c.methodA();
+//     }
+// }
---
+++ review/MultipleInheritanceUsingInterfaces.java
@@ -0,0 +1,28 @@
+interface A{
+    void print();
+}
+interface B{
+    void print();
+}
+interface C extends A, B{
+    void display();
+}
+
+public class MultipleInheritanceUsingInterfaces {
+    public static void main(String[] args) {
+        C obj = new C() {
+
+            @Override
+            public void print() {
+                System.out.println("Print method from both A and B interfaces");
+            }
+
+            @Override
+            public void display() {
+                System.out.println("Display method from C interface");
+            }
+        };
+        obj.print();
+        obj.display();
+    }
+}
