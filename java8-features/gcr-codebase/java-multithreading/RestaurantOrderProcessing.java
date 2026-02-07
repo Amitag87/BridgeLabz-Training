025f64a9a02b42ae0e4cf484e629d57467f463ad
AMIT AGARWAL <amit.agarwal_cs22@gla.ac.in>
2026-02-07 23:23:07 +0530

    [Amit Agarwal]Add.Solved java multithreading Problem

---
+++ java8-features/gcr-codebase/java-multithreading/RestaurantOrderProcessing.java
@@ -0,0 +1,55 @@
+public class RestaurantOrderProcessing {
+    
+    static class Chef extends Thread {
+        private String dishName;
+        private int cookingTimeSeconds;
+        
+        public Chef(String name, String dishName, int cookingTimeSeconds) {
+            super(name);
+            this.dishName = dishName;
+            this.cookingTimeSeconds = cookingTimeSeconds;
+        }
+        
+        @Override
+        public void run() {
+            System.out.println(getName() + " started preparing " + dishName);
+            
+            int[] progressSteps = {25, 50, 75, 100};
+            int sleepTime = (cookingTimeSeconds * 1000) / 4;
+            
+            for (int progress : progressSteps) {
+                try {
+                    Thread.sleep(sleepTime);
+                    System.out.println(getName() + " preparing " + dishName + ": " + progress + "% complete");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Restaurant kitchen opening...\n");
+        
+        Chef chef1 = new Chef("Chef-1", "Pizza", 3);
+        Chef chef2 = new Chef("Chef-2", "Pasta", 2);
+        Chef chef3 = new Chef("Chef-3", "Salad", 1);
+        Chef chef4 = new Chef("Chef-4", "Burger", 2);
+        
+        chef1.start();
+        chef2.start();
+        chef3.start();
+        chef4.start();
+        
+        try {
+            chef1.join();
+            chef2.join();
+            chef3.join();
+            chef4.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        
+        System.out.println("\nKitchen closed - All orders completed");
+    }
+}
---
+++ java8-features/gcr-codebase/java-multithreading/ThreadStateMonitoring.java
@@ -0,0 +1,110 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class ThreadStateMonitoring {
+    
+    static class TaskRunner extends Thread {
+        public TaskRunner(String name) {
+            super(name);
+        }
+        
+        @Override
+        public void run() {
+            try {
+                // TIMED_WAITING state
+                Thread.sleep(2000);
+                
+                // RUNNABLE state - perform computation
+                long sum = 0;
+                for (int i = 0; i < 1000000; i++) {
+                    sum += i;
+                }
+                
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            // TERMINATED state after completion
+        }
+    }
+    
+    static class StateMonitor extends Thread {
+        private List<Thread> threadsToMonitor;
+        private Map<String, List<Thread.State>> stateHistory;
+        
+        public StateMonitor(List<Thread> threads) {
+            this.threadsToMonitor = threads;
+            this.stateHistory = new HashMap<>();
+            for (Thread t : threads) {
+                stateHistory.put(t.getName(), new ArrayList<>());
+            }
+        }
+        
+        @Override
+        public void run() {
+            boolean allTerminated = false;
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+            
+            while (!allTerminated) {
+                allTerminated = true;
+                
+                for (Thread thread : threadsToMonitor) {
+                    Thread.State currentState = thread.getState();
+                    List<Thread.State> history = stateHistory.get(thread.getName());
+                    
+                    if (history.isEmpty() || history.get(history.size() - 1) != currentState) {
+                        history.add(currentState);
+                        String timestamp = LocalTime.now().format(formatter);
+                        System.out.println("[Monitor] " + thread.getName() + " is in " + currentState + " state at " + timestamp);
+                    }
+                    
+                    if (currentState != Thread.State.TERMINATED) {
+                        allTerminated = false;
+                    }
+                }
+                
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            
+            System.out.println("\n=== Summary ===");
+            for (Map.Entry<String, List<Thread.State>> entry : stateHistory.entrySet()) {
+                System.out.println("Summary: " + entry.getKey() + " went through " + entry.getValue().size() + " states");
+                System.out.println("  States: " + entry.getValue());
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        TaskRunner task1 = new TaskRunner("Task-1");
+        TaskRunner task2 = new TaskRunner("Task-2");
+        
+        List<Thread> tasks = Arrays.asList(task1, task2);
+        StateMonitor monitor = new StateMonitor(tasks);
+        
+        System.out.println("Starting Thread State Monitoring System\n");
+        
+        monitor.start();
+        
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        
+        task1.start();
+        task2.start();
+        
+        try {
+            monitor.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        
+        System.out.println("\nMonitoring complete!");
+    }
+}
